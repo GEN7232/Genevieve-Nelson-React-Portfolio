251c1777463cf68df61d15164d90b59c83f79b77
Genevieve Nelson <GEN7232@gmail.com>
2022-09-15 13:39:33 -0400

    contact and home minor changes

--- src/Components/Pages/ContactMe/Contact.js
+++ src/Components/Pages/ContactMe/Contact.js
@@ -1,8 +1,6 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
-import './Contact.css'
-
-
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import "./Contact.css";
 
 const Contact = () => {
   const form = useRef();
@@ -10,26 +8,38 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_mvozjjt', 'template_2o5v1wz', form.current, 'Txo8nMBF9Z5sccG2Z')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_mvozjjt",
+        "template_2o5v1wz",
+        form.current,
+        "Txo8nMBF9Z5sccG2Z"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
   };
 
   return (
-    <form className='form' ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <br>
-      </br>
-      <input className='btn btn-dark' type="submit" value="Send" />
-    </form>
+    <div className="container">
+      <br></br>
+      <h1>Contact Me</h1>
+      <form className="form" ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <br></br>
+        <input className="btn btn-dark" type="submit" value="Send" />
+      </form>
+    </div>
   );
 };
 
