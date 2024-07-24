0c5bd5e912152a7b1890b5a338b1b72609e0fa1d
Genevieve Nelson <GEN7232@gmail.com>
2024-07-23 22:14:34 -0400

    resume page and app color change

--- src/Components/Pages/Resume/Resume.js
+++ src/Components/Pages/Resume/Resume.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import resume from '../../../assets/Resume/Genevieve-Nelson-Resume-2024.png'
-import './Resume.css';
-
+import React from "react";
+import resume from "../../../assets/Resume/Genevieve-Nelson-Resume-2024.png";
+import "./Resume.css";
 
 function Resume() {
-    return (
-        <div className='container'>
-             <a href="Genevieve-Nelson-Resume-2024.pdf" download="Genevieve-Nelson-Resume-2024.pdf">
-                <h1>Click to download my resume!</h1>
-            </a>
-             <a href="Genevieve-Nelson-Resume-2024.pdf" download="Genevieve-Nelson-Resume-2024.pdf">  
-                <img className='res-img' src={resume} alt="Genevieve Nelson Resume" />
-            </a>    
-
-        </div>
-    );
+  return (
+    <div className="container-container">
+      <div className="container">
+        <a
+          href="Genevieve-Nelson-Resume-2024.pdf"
+          download="Genevieve-Nelson-Resume-2024.pdf"
+        >
+          <h1>Click to download my resume!</h1>
+        </a>
+        <a
+          href="Genevieve-Nelson-Resume-2024.pdf"
+          download="Genevieve-Nelson-Resume-2024.pdf"
+        >
+          <img className="res-img" src={resume} alt="Genevieve Nelson Resume" />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Resume;
