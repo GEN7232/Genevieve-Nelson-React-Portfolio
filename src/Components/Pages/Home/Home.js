f6a62ff48c3651c939d07eaf64595ab2ad137069
Genevieve Nelson <GEN7232@gmail.com>
2024-07-28 17:33:45 -0400

    updated bio on home page

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,67 +1,89 @@
-import React from 'react';
+import React from "react";
 // import Profile from './Profile/Profile';
-import Footer from './Footer/Footer';
-import './Home.css';
-import Button from 'react-bootstrap/Button';
+import Footer from "./Footer/Footer";
+import "./Home.css";
+import Button from "react-bootstrap/Button";
 
-function Home( ) {
-    return (
-        <div className='home-container'>
-              <div className='profile-container'>
-            <div className='profile-parent'>
-                <div className='profile-details'>
-                    <div className='colz'>
-                        <div className='colz-icon'>
-                        <a href='https://github.com/GEN7232' target='_blank' rel='noreferrer'>
-                            <i className='fa-brands fa-github'></i>
-                        </a>
-                        <a href='https://www.linkedin.com/in/genevieve-nelson-b13b65186/' target='_blank' rel='noreferrer'>
-                            <i class="fa-brands fa-linkedin-in"></i>
-                        </a>
-                        <a href='https://www.facebook.com/genevieveelizabeth/' target='_blank' rel='noreferrer'>
-                            <i class="fa-brands fa-facebook-f"></i>
-                        </a>
-                        </div>
-                    </div>
-                    <div className='profile-details-name'>
-                        <span className='primary-text'>
-                            {" "}
-                            Hello, I'm <span className='highlighted-text'>Genevieve Nelson</span>
-                        </span>
-                    </div>
-                    <div className='profile-details-role'>
-                        <span className='primary-text'>
-                            {" "}
-                            <h1>
-
-                            </h1>
-                            <span className='profile-role-tagline'>
-                                Making each application rock in its own way!
-                            </span>
-                        </span>
-                    </div>
-                    <div className='profile-options'>
-                        <a href="Genevieve-Nelson-Resume-2024.pdf" download="Genevieve-Nelson-Resume-2024.pdf">
-                            <Button className='button' variant='dark'>
-                                Get Resume
-                            </Button>
-                        </a>
-                    </div>
-                </div>
-                <div className='profile-picture'>
-                    <div className='profile-picture-background'>
-
-                    </div>
-                </div>
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="profile-container">
+        <div className="profile-parent">
+          <div className="profile-details">
+            <div className="colz">
+              <div className="colz-icon">
+                <a
+                  href="https://github.com/GEN7232"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <i className="fa-brands fa-github"></i>
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/genevieve-nelson-b13b65186/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <i class="fa-brands fa-linkedin-in"></i>
+                </a>
+                <a
+                  href="https://www.facebook.com/genevieveelizabeth/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <i class="fa-brands fa-facebook-f"></i>
+                </a>
+              </div>
             </div>
+            <div className="profile-details-name">
+              <span className="primary-text">
+                {" "}
+                Hello, I'm{" "}
+                <span className="highlighted-text">Genevieve Nelson</span>
+              </span>
+            </div>
+            <div className="profile-details-role">
+              <span className="primary-text">
+                {" "}
+                <h1></h1>
+                <span className="profile-role-tagline">
+                  Making each application rock in its own way!
+                </span>
+              </span>
+            </div>
+            <div className="profile-options">
+              <a
+                href="Genevieve-Nelson-Resume-2024.pdf"
+                download="Genevieve-Nelson-Resume-2024.pdf"
+              >
+                <Button className="button" variant="dark">
+                  Get Resume
+                </Button>
+              </a>
+            </div>
+          </div>
+          <div className="profile-picture">
+            <div className="profile-picture-background"></div>
+          </div>
         </div>
-            {/* <Profile /> */}
-            <Footer />
-            <p>
-                 Hello, I'm Genevieve Nelson, and I am a fullstack web developer proficient in HTML, CSS, Javascript, and MERN programming. I initially went to school for Audio Engineering at the Unviersity of New Haven, and spent several years working out of recording studios in New York and New Jersey. I like to use the skills I learned in the studio and apply them to programming. In particular I love being both creative and technical at the same time. I find it to be incredibly engaging. I am also very good at troubleshooting after years of working with vintage audio gear and recording software.
-            </p>
-        </div>
-    );
+      </div>
+      {/* <Profile /> */}
+      <Footer />
+      <p>
+        Hello, I'm Genevieve Nelson, and I am a fullstack web developer. I have
+        experience programming utilizing HTML5, CSS3, JavaScript, jQuery,
+        Express.js, React.js, Node.js, Vue.js, Python, Visual Basic, .NET, C#,
+        ASP, progressive web apps, agile methodology, computer science, database
+        theory, MongoDB, MySQL, Git, and more. I originally went to school to be
+        an audio engineer, and worked in that field for several years before
+        attending a coding bootcamp hosted by Rutgers University in 2022. Since
+        then I have been working in technology as a web and software developer.
+        I like to use the skills I learned in the studio to make myself a better
+        developer. That extends to troubshooting, balancing creativity with
+        technical skills, client relations, and more.
+      </p>
+    </div>
+  );
 }
 
 export default Home;
